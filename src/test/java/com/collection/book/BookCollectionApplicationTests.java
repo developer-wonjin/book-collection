39c856f84d76ddfc806c351c25b4113f75c727ef
wonjin1776 <wonjin1776@naver.com>
2024-12-26 00:06:15 +0900

    initial commit

---
+++ src/test/java/com/collection/book/BookCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collection.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
